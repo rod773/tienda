a45fc56eaa3f41c96951a9c0f0d928773762665b
rod773 <104315743+rod773@users.noreply.github.com>
2023-07-14 13:08:19 +0200

    product card

--- index.php
+++ index.php
@@ -5,13 +5,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Tienda</title>
 </head>
 
 <body>
-    <?php
-    echo "<h1>Tienda</h1";
-    ?>
+    <nav class="nav">
+        <div class="navbar-brand d-flex justify-content-center">
+            <img src="./img/logo.jpg" alt="logo" id=logo>
+            <span>Logo</span>
+        </div>
+        <a class="nav-link" href="#">Home</a>
+        <a class="nav-link" href="#">Carrito</a>
+    </nav>
+    <div class="container">
+        <div class="alert alert-success">
+            <a href="" class="badge badge-success">Ver Carrito</a>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <div class="card">
+                    <img class="card-img-top" title="titulo del producto" src="./img/titulo.jpg" alt="titulo">
+                    <div class="card-body">
+                        <span>Titulo del producto</span>
+                        <h5 class="card-title">€300.00</h5>
+                        <p class="card-text">Descripción</p>
+                        <button class="btn btn-primary" type="submit" name="btnAccion" value="agregar">Agregar al
+                            Carrito</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="./js/jquery-3.2.1.slim.min.js"></script>
     <script src="./js/popper.min.js"></script>
     <script src="./js/bootstrap.min.js"></script>
