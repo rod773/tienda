4cf9e63d7ce5cb20b71cc62cfc90e80ece98340f
rod773 <104315743+rod773@users.noreply.github.com>
2023-07-14 11:18:08 +0200

    instalacion bootstrap 4

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
-<?php 
-echo "<h1>Tienda</h1";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <title>Tienda</title>
+</head>
+
+<body>
+    <?php
+    echo "<h1>Tienda</h1";
+    ?>
+    <script src="./js/jquery-3.2.1.slim.min.js"></script>
+    <script src="./js/popper.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+</body>
+
+</html>
